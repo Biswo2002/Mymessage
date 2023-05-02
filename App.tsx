d767988d3f563bdebcca1471cac460bb118dcb77
Biswopaban Nayak <biswopabannayak@gmail.com>
2023-05-02 23:43:26 +0530

    Added WelCome Screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { StyleSheet } from 'react-native'
 import React from 'react'
 import Routes from './src/Routes'
+import { NativeBaseProvider, theme } from 'native-base'
+import { NavigationContainer } from '@react-navigation/native'
 
 const App = () => {
   return (
-    <Routes />
+    <NativeBaseProvider theme={theme as any} >
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    </NativeBaseProvider>
   )
 }
 
