c33d36a705255f7dcc56eb1174e592a1923a41a4
Biswopaban Nayak <biswopabannayak@gmail.com>
2023-05-02 23:43:47 +0530

    Added WelCome Screen

--- src/routes/public/PublicRoutes.tsx
+++ src/routes/public/PublicRoutes.tsx
@@ -1,14 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { StyleSheet } from 'react-native'
 import React from 'react'
+import Onboarding from '../../screen/public/Onboarding';
+import Welcome from '../../screen/Auth/Welcome';
+
+
 
 const PublicRoutes = () => {
+  const Stack = createNativeStackNavigator();
   return (
-    <View>
-      <Text>PublicRoutes</Text>
-    </View>
+    <Stack.Navigator
+      initialRouteName={'Onboarding'}
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name='Onboarding' component={Onboarding} />
+      <Stack.Screen name='Welcome' component={Welcome} />
+    </Stack.Navigator>
   )
 }
-
 export default PublicRoutes
 
 const styles = StyleSheet.create({})
