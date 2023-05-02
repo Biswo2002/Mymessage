2880505e1c178af85bc464eaf9fbd28b86dfb349
Biswopaban Nayak <biswopabannayak@gmail.com>
2023-05-02 23:43:34 +0530

    Added WelCome Screen

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,10 @@
 import { StyleSheet } from 'react-native'
 import React from 'react'
-import { PrivateRoutes } from './routes/private'
+import PublicRoutes from './routes/public/PublicRoutes'
 
 const Routes = () => {
     return (
-        <PrivateRoutes />
+        <PublicRoutes />
     )
 }
 
